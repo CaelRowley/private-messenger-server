883a6f2b3392c59f724c2d26b4a62897841252fc
Cael Rowley <caelrowley@gmail.com>
2018-12-21 22:30:05 +0000

    Updated tests to es6

--- tests/controllers/healthcheck_test.js
+++ tests/controllers/healthcheck_test.js
@@ -2,9 +2,9 @@ import chai from "chai";
 import * as healthcheckController from "../../src/controllers/healthcheck"
 const should = chai.should();
 
-describe("healthcheckController test suite", function () {
-  context("healthcheckController.healthcheck()", function () {
-    it('should return "Health check!"', function () {
+describe("healthcheckController test suite", () => {
+  context("healthcheckController.healthcheck()", () => {
+    it('should return "Health check!"', () => {
       healthcheckController.healthcheck().should.equal("Health check!");
     });
   });
