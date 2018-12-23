3fd89c2c4ea19c4ec13038dfba29669b1b62e737
Cael Rowley <caelrowley@gmail.com>
2018-12-23 17:54:48 +0000

    Moved socket listeners to different file

--- src/app.js
+++ src/app.js
@@ -2,25 +2,14 @@ import express from 'express';
 import http from 'http';
 import socketIO from 'socket.io';
 import { addRouters } from './routes/app-router';
+import { addSockets } from './sockets/app-sockets';
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
 addRouters(app);
-
-io.on('connection', socket => {
-  console.log('Client connected');
-
-  socket.on('emit message', (message) => {
-    console.log(`Emitting message: "${message}" to all clients`);
-    io.sockets.emit('emit message', message);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-});
+addSockets(io);
 
 server.listen(process.env.PORT, () => {
   console.log(`Server be jammin' on port ${process.env.PORT}!`);
